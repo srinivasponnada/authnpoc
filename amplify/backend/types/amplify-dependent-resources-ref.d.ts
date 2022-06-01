5db3c99d06929175c98e8cca4c2bd61ec43f265c
Srinivas Ponnada <sponnada@hotmail.com>
2022-06-01 12:31:25 +0530

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "authnpoc7e34b6c3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ android/app/src/main/kotlin/com/example/authnpoc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.authnpoc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
